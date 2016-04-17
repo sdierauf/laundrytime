57af779f2967be06a0030025ac24a85dea5cc7f2
gergf <germanmontcada@gmail.com>
2016-04-17 19:38:15 +0200

    Modals connected with db

--- app/js/modalCtrl.js
+++ app/js/modalCtrl.js
@@ -1,19 +1,55 @@
 laundryTimeApp.controller('modalCtrl', function($scope, modalFactory) {
-	/* Report modal */
-	$scope.reportSelector = modalFactory.reportSelector;
 
-	/* User info */
-	$scope.userInfo = modalFactory.userInfo; 
+	$scope.modalFactory = modalFactory;
 
-	$scope.reportModSelChanged = function () {
-		$scope.reportSelector.show = ($scope.reportSelector.selected == 5); 
+	/* handle functions */
+	var handleSuccess = function(data){
+		console.log("Success: ");
+		console.log(data); 
 	}
 
-	$scope.checkPIN = function () {
-		/* send PIN to db here */
+	var handleError = function(data){
+		console.log("Error in modalCtrl.handleError():"); 
+		console.log(data); 
+	}
+	
+	/* functions */
+	$scope.reportModSelChanged = function () {
+		$scope.modalFactory.reportSelector.show = 
+		($scope.modalFactory.reportSelector.selected == 5); 
 	}
 
 	$scope.saveUserCookies = function(){
 		modalFactory.saveUserCookies(); 
 	}
+
+	/* html calls */
+
+	/* POST REQUEST */
+	$scope.addUserToQueue = function(){
+		modalFactory.addUserToQueue().
+		then(handleSuccess, handleError); 
+		$scope.getQueue('Machine 1'); 
+	}
+
+	/* GET REQUEST */
+	$scope.getQueue = function(machineName){
+		modalFactory.getQueue(machineName).then(
+		/* Success */
+		function(obj){
+			var queue = [];
+			for(var i = 0; i < obj.data.queue.length; i++){
+				queue.push(obj.data.queue[i]);
+			} 
+			modalFactory.setMachineQueue(queue);  
+			console.log(obj);
+			console.log("Modal has got the queue machine successfully."); 
+		}
+		/* Erorr */
+			, function(obj){
+				console.log("Error in 'modalCtrl.getQueue()': "); 
+				console.log(obj); 
+			});
+	}
+
 });/* endController */
--- app/js/modalFactory.js
+++ app/js/modalFactory.js
@@ -1,8 +1,10 @@
-laundryTimeApp.factory('modalFactory', function($cookieStore) {
+laundryTimeApp.factory('modalFactory', function($cookieStore, $http) {
 
-	/* Load cookies */
 	var cookReportSelector = $cookieStore.get('reportSelector'); 
 	var cookUserInfo = $cookieStore.get('userInfo'); 
+
+	/* */
+	this.machineQueue = [];
 
 	/* if there is not value stored in cookies */
 	if(cookReportSelector === undefined){
@@ -24,12 +26,37 @@ laundryTimeApp.factory('modalFactory', function($cookieStore) {
 		this.userInfo = cookUserInfo; 
 	}
 
-	/* end Cookies */
-
+	/* cookies */
 	this.saveUserCookies = function(){
 		$cookieStore.put('userInfo', this.userInfo);
 	}
+	/* end Cookies */
 
+	/* http requests */
+	this.addUserToQueue = function(){
+		var req = $http({
+			method: 'POST', 
+			url: '/machines/Machine 1/queue',
+			data: {
+				user: this.userInfo.email,
+				minutes: 20, 
+				pin: 0000, 
+			}
+		});
+
+		return req; 
+	}
+
+	this.getQueue = function(machine){
+		return $http.get('/machines/'+machine+'/queue');
+	}
+
+	/* Setters and Getters */
+
+	this.setMachineQueue = function(newValue){
+		this.machineQueue = newValue; 
+	}
+	
 	/* return the service */
 	return this;	
 });
