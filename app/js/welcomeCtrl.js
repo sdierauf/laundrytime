384be496f137ec799f20a40cdb871fe0d4bfcef6
Stefan Dierauf <sdierauf@gmail.com>
2016-04-24 19:13:25 +0200

    git blame welcome.html

--- app/js/welcomeCtrl.js
+++ app/js/welcomeCtrl.js
@@ -1,18 +1,18 @@
 
 //Unused for now
 laundryTimeApp.controller('welcomeCtrl', function ($scope, $routeParams) {
-	$scope.getAllResidences = function(){
-		var xmlHttp = new XMLHttpRequest()
-		xmlHttp.open( "GET", "http://0.0.0.0:8080/machines", true ) //Async set to false
+	// $scope.getAllResidences = function(){
+	// 	var xmlHttp = new XMLHttpRequest()
+	// 	xmlHttp.open( "GET", "http://0.0.0.0:8080/machines", true ) //Async set to false
 
-		xmlHttp.send()
-		var response = xmlHttp.responseText
+	// 	xmlHttp.send()
+	// 	var response = xmlHttp.responseText
 
-		console.log(all_machines)
-		return all_machines
-	}
+	// 	console.log(all_machines)
+	// 	return all_machines
+	// }
 
-	$scope.residences = $scope.getAllResidences()
+	// $scope.residences = $scope.getAllResidences()
 
 });
 
